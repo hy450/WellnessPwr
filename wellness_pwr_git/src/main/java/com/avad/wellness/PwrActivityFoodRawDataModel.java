67b65f6f91bc7334378948b4f7440ba5b4aa56ca
hy450 <hy450@avad.co.kr>
2016-08-27 15:50:37 +0900

    json 예시 데이터 추가

---
+++ wellness_pwr_git/src/main/java/com/avad/wellness/PwrActivityFoodRawDataModel.java
@@ -0,0 +1,27 @@
+package com.avad.wellness;
+
+public class PwrActivityFoodRawDataModel {
+	String food_logged_id;	
+	int starttime;	
+	int meal_type; // BREAKFAST =1 ,LUNCH=2,DINNER=3,MORNING_SNACK=4,	AFTERNOON_SNACK=5,EVENING_SNACK=6
+	String food_dtl_logged_list;	
+	double calorie;
+	double total_fat;
+	double saturated_fat;
+	double polysaturated_fat;
+	double monosaturated_fat;
+	double unsaturated_fat;
+	double trans_fat;
+	double carbohydrate;
+	double dietary_fiber;
+	double sugar;
+	double protein;
+	double cholestrol;
+	double sodium;
+	double potassium;
+	double vitamin_c;
+	double vitamin_d;
+	double calcium;
+	double iron;
+
+}
---
+++ wellness_pwr_git/src/main/java/com/avad/wellness/PwrActivityFoodRawDtlDataModel.java
@@ -0,0 +1,39 @@
+package com.avad.wellness;
+
+public class PwrActivityFoodRawDtlDataModel {
+	String food_dtl_logged_id;
+	String provider_id;
+	String provider_description;
+	int starttime;
+	int inputtime;
+	int savetime;
+	String food_pic;  //delim ","
+	int meal_type; // BREAKFAST =1 ,LUNCH=2,DINNER=3,MORNING_SNACK=4,	AFTERNOON_SNACK=5,EVENING_SNACK=6
+	String food_name;
+	String food_brand;
+	int main_cat;
+	int middle_cat;
+	int sub_cat;
+	double amount;
+	double calorie;
+	double total_fat;
+	double saturated_fat;
+	double polysaturated_fat;
+	double monosaturated_fat;
+	double unsaturated_fat;
+	double trans_fat;
+	double carbohydrate;
+	double dietary_fiber;
+	double sugar;
+	double protein;
+	double cholestrol;
+	double sodium;
+	double potassium;
+	double vitamin_c;
+	double vitamin_d;
+	double calcium;
+	double iron;	
+	
+
+
+}
